39d25f7724f534533c8b91a9addf02c21809abed
Ewan Willsmer <ewanwillsmer@gmail.com>
2022-02-03 08:51:06 +0000

    Add Local API Events file

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import Layout from "@/components/layout";
+import { API_URL } from "@/config/index";
 
-export default function HomePage() {
+export default function HomePage({ events }) {
   return (
     <Layout>
       <h1>Home</h1>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1, // checks for any changes every 1 sec
+  };
 }
